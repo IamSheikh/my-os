92fb99ea518920a2490a3b6880294911c486c5f0
TahiJameel <tahajameel.co@gmail.com>
2021-08-28 19:45:44 +0500

    add login-form-img class to img

--- public/LoginPage.js
+++ public/LoginPage.js
@@ -4,7 +4,7 @@ class LoginPage extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
             <form id="loginForm">
-                <img src="./img/avatar.png" alt=${window.name} class="main-img" />
+                <img src="./img/avatar.png" alt=${window.name} class="login-form-img" />
                 <h3 class="main-text">${window.name}</h3>
                 <input type="password" name="userpassword" id="userpassword" placeholder="Enter your password..." class="main-input" />
                 <button class="main-btn">Login</button>
